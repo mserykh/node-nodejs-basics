f97a434634b7ff6153f60d3320e9b3cc82a37e69
Maria Serykh <marie.serykh@gmail.com>
2022-06-06 00:13:38 +0200

    feat: implement reading and writing to a file using streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,12 @@
+import fs from "fs";
+
 export const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream("./files/fileToRead.txt", "utf-8");
+  let data = "";
+  stream.on("data", (chunk) => {
+    data += chunk;
+  });
+  stream.on("end", () => process.stdout.write(data));
 };
+
+await read();
