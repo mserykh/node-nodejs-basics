491663e0616117179404c2047d5094c4e6749547
Maria Serykh <marie.serykh@gmail.com>
2022-06-05 14:06:31 +0200

    feat: implement logging filtered environment arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,29 @@
-export const parseArgs = () => {
-    // Write your code here 
+const filterEnvArguments = (envArray) => {
+  return envArray.filter((k, i, array) => {
+    if (k.startsWith("--")) return true;
+    if (i > 0 && array[i - 1].startsWith("--")) return true;
+  });
 };
+
+const printEnvArguments = (envArguments) => {
+  if (envArguments.length === 0) {
+    console.log("No arguments were found");
+    return;
+  }
+  const newArgumentsArray = [];
+  envArguments.forEach((el, i) => {
+    if (i % 2 === 0) {
+      newArgumentsArray.push(`${el.slice(2)} is ${envArguments[i + 1]}`);
+    }
+  });
+  const stringToPrint = newArgumentsArray.join(", ");
+  console.log(stringToPrint);
+};
+
+export const parseArgs = () => {
+  const envArguments = process.argv;
+  const envArgumentsFiltered = filterEnvArguments(envArguments);
+  printEnvArguments(envArgumentsFiltered);
+};
+
+parseArgs();
