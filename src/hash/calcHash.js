3b88e54640ffcf59b18c40a23fee5dbd2c5d052b
Maria Serykh <marie.serykh@gmail.com>
2022-06-05 23:17:17 +0200

    feat: implement calculating hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
+import fs from "fs/promises";
+import crypto from "crypto";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const data = await fs.readFile("./files/fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256");
+  if (data) {
+    hash.update(data);
+  }
+  const hex = hash.digest("hex");
+  return hex;
 };
+
+await calculateHash();
