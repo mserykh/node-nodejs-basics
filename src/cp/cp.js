5e2718553a8e7926d0ff0dce6c8fcca19cc52502
Maria Serykh <marie.serykh@gmail.com>
2022-06-06 16:38:10 +0200

    feat: implement function `spawnChildProcess`

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,10 @@
+import { fork } from "child_process";
+import path from "path";
+
+const modulePath = path.resolve("files", "script.js");
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(modulePath, args);
 };
+
+await spawnChildProcess(process.argv.slice(2));
