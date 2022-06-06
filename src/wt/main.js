02ad57c8349ba8ea12cf88949ff909593e75df52
Maria Serykh <marie.serykh@gmail.com>
2022-06-06 15:09:32 +0200

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,43 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
+const workerFilePath = "./worker.js";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpuCount = os.cpus().length;
+  const result = [];
+  const promises = [];
+
+  for (let i = 0; i < cpuCount; i++) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const workerInput = i + 10;
+        const worker = new Worker(workerFilePath, {
+          workerData: workerInput,
+        });
+        const workerResult = { status: "created", data: null };
+        result.push(workerResult);
+
+        worker.on("message", (value) => {
+          workerResult.status = "resolved";
+          workerResult.data = value;
+          console.log(`thread ${i}: resolved`);
+          resolve();
+        });
+
+        worker.on("error", () => {
+          workerResult.status = "error";
+          console.log(`thread ${i}: error`);
+          resolve();
+        });
+      })
+    );
+  }
+
+  await Promise.all(promises);
+
+  return result;
 };
+
+const result = await performCalculations();
+console.log(result);
