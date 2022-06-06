c9adb479b65cfb0911bc5f77a785b83a395906ea
Maria Serykh <marie.serykh@gmail.com>
2022-06-06 17:53:00 +0200

    refactor: refactor `spawnChildProcess` using `spawn` method

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,10 +1,21 @@
-import { fork } from "child_process";
+import { spawn } from "child_process";
 import path from "path";
 
 const modulePath = path.resolve("files", "script.js");
 
 export const spawnChildProcess = async (args) => {
-  fork(modulePath, args);
+  const child = spawn("node", [modulePath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  child.on("error", (error) => {
+    console.log(error);
+    process.exit(1);
+  });
 };
 
 await spawnChildProcess(process.argv.slice(2));
