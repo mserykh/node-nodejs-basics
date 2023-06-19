83817f1c715fa3e062d29cd3e751ebf18f2add47
Maria Serykh <61358937+mserykh@users.noreply.github.com>
2023-06-19 21:27:21 +0200

    feat: implement renaming a file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import { rename as renameFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { open } from "fs";
+
+const filePath = fileURLToPath(import.meta.url);
+const dirName = dirname(filePath);
+const oldFileName = "wrongFilename.txt";
+const newFileName = "properFilename.md";
+
 export const rename = async () => {
-    // Write your code here 
+  open(join(dirName, "files", newFileName), (err) => {
+    if (!err) {
+      throw new Error("FS Operation failed");
+    }
+    renameFile(
+      join(dirName, "files", oldFileName),
+      join(dirName, "files", newFileName)
+    );
+  });
 };
+
+await rename();
