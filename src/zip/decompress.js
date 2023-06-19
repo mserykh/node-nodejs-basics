377db770fc0831e883870386c9e22b104d8927f2
Maria Serykh <61358937+mserykh@users.noreply.github.com>
2023-06-19 23:23:10 +0200

    feat: implement compressing and decompressing files

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import { createUnzip } from "zlib";
+
+const dirPath = fileURLToPath(import.meta.url);
+const dirName = dirname(dirPath);
+const originalFilePath = join(dirName, "files", "archive.gz");
+const restoredFilePath = join(dirName, "files", "fileToCompress.txt");
+
 export const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  const originalFile = createReadStream(originalFilePath);
+  const restoredFile = createWriteStream(restoredFilePath);
+
+  pipeline(originalFile, unzip, restoredFile, (error) => {
+    if (error) {
+      console.log(error);
+      process.exitCode = 1;
+    }
+  });
 };
+
+await decompress();
