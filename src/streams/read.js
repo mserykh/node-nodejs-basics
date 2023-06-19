b6704315f8d04ef287f5c2df8d6f2dde295b1f32
Maria Serykh <61358937+mserykh@users.noreply.github.com>
2023-06-19 23:17:51 +0200

    feat: implement streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const dirPath = fileURLToPath(import.meta.url);
+const dirName = dirname(dirPath);
+const filePath = join(dirName, "files", "/fileToRead.txt");
+
 export const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath, "utf-8");
+  let data = "";
+  stream.on("data", (chunk) => {
+    data += chunk;
+  });
+  stream.on("end", () => process.stdout.write(data));
 };
+
+await read();
