0024b6cf11df2dae725ad6c9ff31c2636bfd8d61
Maria Serykh <61358937+mserykh@users.noreply.github.com>
2023-06-19 21:57:23 +0200

    feat: implement listing file names

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,31 @@
+import { open } from "fs";
+import { readdir, stat } from "fs/promises";
+import { dirname, join, parse } from "path";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(import.meta.url);
+const dirName = dirname(filePath);
+const filesDirPath = join(dirName, "files");
+
+async function isFile(path) {
+  const stats = await stat(path);
+
+  return stats.isFile();
+}
+
 export const list = async () => {
-    // Write your code here 
+  open(filesDirPath, async (err) => {
+    if (err) {
+      throw new Error("FS Operation failed");
+    }
+
+    const files = await readdir(filesDirPath, { withFilesType: true });
+    for (let file of files) {
+      if (await isFile(join(filesDirPath, file))) {
+        console.log(parse(file).name);
+      }
+    }
+  });
 };
+
+await list();
