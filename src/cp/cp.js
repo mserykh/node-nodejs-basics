00e4a7bf1c04209fb6f576576cbccca9fa420466
Maria Serykh <61358937+mserykh@users.noreply.github.com>
2023-06-19 23:40:51 +0200

    feat: implement child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,24 @@
+import { spawn } from "child_process";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const dirPath = fileURLToPath(import.meta.url);
+const dirName = dirname(dirPath);
+const modulePath = join(dirName, "files", "script.js");
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn("node", [modulePath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  child.on("error", (error) => {
+    console.log(error);
+    process.exit(1);
+  });
 };
+
+spawnChildProcess(["arg1", "arg2", "arg3"]);
